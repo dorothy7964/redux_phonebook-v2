1394db311fad59d38e7f8f5a5503de250e59e883
SEC\user <dorothy_007@naver.com>
2019-07-04 01:13:22 +0900

    리덕스 개발자도구 적용

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import { createStore } from 'redux';
 import rootReducer from './store/modules';
 
 import { Provider } from 'react-redux';
-const store = createStore(rootReducer);
+
+// **** 리덕스 개발자도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
